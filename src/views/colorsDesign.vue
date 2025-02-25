027fc4d2b4c25cf46851c4a5f34f50ba1b730a1e
KrzyKerlin <krzysztof.kerlin@op.pl>
2025-02-25 16:06:20 +0100

    Colors Design- add icon color view

--- src/views/colorsDesign.vue
+++ src/views/colorsDesign.vue
@@ -4,8 +4,11 @@
     <v-row class="fill-height mx-2 md-mx-8">
       <v-col class="d-flex flex-column align-center justify-center" :style="{ backgroundColor: bgColor }" cols="12" md="8">
         <h1 :style="{ color: textColor}" class="text-h3 text-md-h1 text-center font-weight-bold my-2">HEADING TEXT</h1>
-        <p :style="{ color: textColor}" class="text-h6 text-md-h5 my-2">Description text</p>
-        <v-btn :style="{ backgroundColor: buttonBgColor, color: buttonTextColor }" size="large"> Preview Button </v-btn>
+        <p :style="{ color: textColor}" class="text-h6 text-md-h5 text-center my-2">Text Description Lorem Ipsum </p>
+        <v-btn :style="{ backgroundColor: buttonBgColor, color: buttonTextColor }" size="large">Button  
+          <v-icon icon="mdi-arrow-right" end></v-icon>
+        </v-btn>
+        <v-icon :color="socialColor" size="40" class="mt-2">mdi-youtube</v-icon>
       </v-col>
   
       <!-- Color Pickers -->
@@ -13,7 +16,7 @@
         <v-card class="pa-6 text-center bg-white lighten-3 elevation-10 rounded-lg">
           <v-row class="mb-2">
             <v-col cols="6" class="d-flex flex-column align-center">
-              <label for="bgColor" style="white-space: nowrap;" >Bg Color</label>
+              <label for="bgColor" style="white-space: nowrap;">Bg Color</label>
               <input type="color" id="bgColor" v-model="bgColor" class="color-picker" />
             </v-col>
             <v-col cols="6" class="d-flex flex-column align-center">
@@ -28,13 +31,17 @@
             <v-col cols="6" class="d-flex flex-column align-center"> 
               <label for="buttonTextColor" style="white-space: nowrap;">Button Txt</label> 
               <input type="color" id="buttonTextColor" v-model="buttonTextColor" class="color-picker" /> 
-            </v-col> 
+            </v-col>
+            <!-- Icons Color Picker -->
+            <v-col cols="12" class="d-flex flex-column align-center">
+              <label for="socialColor" style="white-space: nowrap;">Icon</label>
+              <input type="color" id="socialColor" v-model="socialColor" class="color-picker" />
+            </v-col>
           </v-row>
           <!-- Reset Button --> 
           <v-btn color="error" @click="resetColors">Reset Colors</v-btn>
         </v-card>
       </v-col>
-
     </v-row>
   </v-container>
 </template>
@@ -49,12 +56,14 @@
         textColor: '#f457ff',
         buttonBgColor: '#2575fc', 
         buttonTextColor: '#ffffff',
+        socialColor: '#1877f2'
       };
   
       const bgColor = ref(defaultColors.bgColor);
       const textColor = ref(defaultColors.textColor);
       const buttonBgColor = ref(defaultColors.buttonBgColor); 
       const buttonTextColor = ref(defaultColors.buttonTextColor);
+      const socialColor = ref(defaultColors.socialColor);
   
       // Apply the colors when the component initializes
       onMounted(() => {
@@ -67,15 +76,16 @@
       });
   
       // Track changes in color values
-      watch([bgColor, textColor, buttonBgColor, buttonTextColor], () => {
+      watch([bgColor, textColor, buttonBgColor, buttonTextColor, socialColor], () => {
         document.body.style.background = bgColor.value;
       }, { immediate: true });
 
-      const resetColors = () => { 
-        bgColor.value = defaultColors.bgColor; 
-        textColor.value = defaultColors.textColor; 
-        buttonBgColor.value = defaultColors.buttonBgColor; 
-        buttonTextColor.value = defaultColors.buttonTextColor; 
+      const resetColors = () => {
+        bgColor.value = defaultColors.bgColor;
+        textColor.value = defaultColors.textColor;
+        buttonBgColor.value = defaultColors.buttonBgColor;
+        buttonTextColor.value = defaultColors.buttonTextColor;
+        socialColor.value = defaultColors.socialColor;
       };
   
       return {
@@ -83,7 +93,8 @@
         textColor,
         buttonBgColor, 
         buttonTextColor,
-        resetColors,
+        socialColor,
+        resetColors
       };
     }
   };
@@ -91,8 +102,8 @@
   
 <style scoped>
   .color-picker {
-    width: 4rem;
-    height: 4rem;
+    width: 3rem;
+    height: 3rem;
     cursor: pointer;
   }
   
