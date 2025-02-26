1f00e531b8a847520819332a31d54d4247703e81
KrzyKerlin <krzysztof.kerlin@op.pl>
2025-02-26 17:02:13 +0100

    Show and copy button css code

--- src/views/buttons.vue
+++ src/views/buttons.vue
@@ -3,36 +3,33 @@
     <BackButton />
       <h1 class="my-4 text-center text-primary">Buttons</h1>
     <v-row>
-      <v-col cols="6" md="3" class="d-flex flex-column align-center my-2">
-        <v-btn class="simple-button">SIMPLE</v-btn>
+      <v-col v-for="(button, index) in buttons" :key="index" cols="6" md="3" class="d-flex flex-column align-center">
+        <v-btn :class="button.class" class="my-4" @click="showCode(button)">
+          {{ button.label }}
+        </v-btn>
       </v-col>
-      <v-col cols="6" md="3" class="d-flex flex-column align-center my-2">
-        <v-btn class="gradient-button">GRADIENT</v-btn>
-      </v-col>
-      <v-col cols="6" md="3" class="d-flex flex-column align-center my-2">
-        <v-btn class="shadow-button">SHADOW</v-btn>
-      </v-col>
-      <v-col cols="6" md="3" class="d-flex flex-column align-center my-2">
-        <v-btn class="scale-button">SCALE</v-btn>
-      </v-col>
-      <v-col cols="6" md="3" class="d-flex flex-column align-center my-2">
-        <v-btn class="slow-hover-button">SLOW</v-btn>
-      </v-col>
-      <v-col cols="6" md="3" class="d-flex flex-column align-center my-2">
-        <v-btn class="glow-button">GLOW</v-btn>
-      </v-col>
-      <v-col cols="6" md="3" class="d-flex flex-column align-center my-2">
-        <v-btn class="button-3d">3D</v-btn>
-      </v-col>
-      <v-col cols="6" md="3" class="d-flex flex-column align-center my-2">
-        <v-btn class="outline-button">OUTLINE</v-btn>
+    </v-row>
+    <v-row v-if="selectedButton" class="d-flex justify-center align-center">
+      <v-col cols="12" md="6">
+        <v-card>
+          <v-card-text>
+              <pre class="text-wrap text-caption">{{ selectedButtonCode }}</pre>
+              <v-btn 
+                class="absolute top right mt-3"
+                color="primary"
+                @click="copyToClipboard(selectedButtonCode)"
+              >
+                <v-icon>mdi-content-copy</v-icon> Copy Code
+              </v-btn>
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import { onMounted, onUnmounted } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 
 export default {
   setup() {
@@ -44,7 +41,77 @@ export default {
     // Reset the background when the component is removed
     onUnmounted(() => {
       document.body.style.background = '';
-    }); 
+    });
+
+    const buttons = ref([
+        { label: 'Simple Btn', class: 'simple-button', css:`
+        .simple-button { background-color: #007BFF; color: white; }
+        .simple-button:hover { background-color: #0056b3; }` },
+        { label: 'Gradient', class: 'gradient-button', css:`
+        .gradient-btn { background: linear-gradient(to right, #0000ff, #09cbfb); color: black; transition: 0.3s ease-in-out; }
+        .gradient-btn:hover { background-color: #0056b3; }` },
+        { label: 'Shadow', class: 'shadow-button', css:`.shadow-button { background-color: #333; color: white; box-shadow: 0px  4px 6px rgba(0, 0, 0, 0.1); transition: box-shadow 0.3s ease; }
+        .shadow-button:hover { box-shadow: 0px 8px 12px rgba(0, 0, 0, 0.2); }` },
+        { label: 'Scale', class: 'scale-button',  css:`.scale-button { background-color: #28a745; color: white; transition: transform 0.3s ease;}
+        .scale-button:hover { transform: scale(1.1); } }
+        .shadow-button:hover { box-shadow: 0px 8px 12px rgba(0, 0, 0, 0.2); }` },
+        { label: 'Slow Hover', class: 'slow-hover-button',  css:`.slow-hover-button { background-color: #17a2b8; color: white;transition: background-color 1s ease; }
+        .slow-hover-button:hover { background-color: #138496; }` },
+        { label: 'Glow', class: 'glow-button',  css:`.glow-button { background-color: #6610f2; color: white; box-shadow: 0 0 5px #6610f2, 0 0 15px #6610f2, 0 0 30px #6610f2, 0 0 50px #6610f2; transition: box-shadow 0.3s ease; }
+        .glow-button:hover { box-shadow: 0 0 20px #6610f2, 0 0 40px #6610f2, 0 0 60px #6610f2, 0 0 100px #6610f2; }` },
+        { label: '3D', class: 'button-3d', css:`.button-3d { background-color: #17a2b8; color: white; box-shadow: 0 5px #138496, 0 10px #117a8b; transition: box-shadow 0.3s ease; }
+        .button-3d:hover { box-shadow: 0 2px #138496, 0 5px #117a8b; }` },
+        { label: 'Outline', class: 'outline-button', css:`.outline-button { background-color: transparent; border: 2px solid #007BFF; color: #007BFF; transition: background-color 0.3s ease; }
+        .outline-button:hover { background-color: #007BFF; color: white; }` }
+    ]);
+
+    const selectedButton = ref(null);
+    const selectedButtonCode = ref('');
+
+    const showCode = (button) => {
+      selectedButton.value = button;
+      selectedButtonCode.value = button.css.trim();
+    };
+
+    // Copy Button CSS code
+    const copyToClipboard = () => {
+      // Check if writeText is supported
+      if (navigator.clipboard && navigator.clipboard.writeText) {
+        navigator.clipboard.writeText(selectedButtonCode.value).then(() => {
+        const button = document.getElementById('copyCode');
+        if (button) {
+          button.textContent = 'Copied :)';
+          setTimeout(() => {
+            button.innerHTML = '<i class="fas fa-copy"></i> Copy Code';
+          }, 2000);
+        }
+      });
+      } else {
+        // Fallback method for browsers that do not support writeText
+        const textArea = document.createElement('textarea');
+        textArea.value = selectedButtonCode.value;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textArea);
+
+        const button = document.getElementById('copyCode');
+        if (button) {
+          button.textContent = 'Copied :)';
+          setTimeout(() => {
+            button.innerHTML = '<i class="fas fa-copy"></i> Copy Code';
+          }, 2000);
+        }
+      }
+    };
+
+    return {
+      buttons,
+      selectedButton,
+      selectedButtonCode,
+      showCode,
+      copyToClipboard
+    };
   }
 }
 
