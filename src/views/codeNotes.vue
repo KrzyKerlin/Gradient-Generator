127e6e05723ec599cbb5b70586f0f7c893a509be
KrzyKerlin <krzysztof.kerlin@op.pl>
2025-03-06 09:48:48 +0100

    style: note card and dialog view changes

--- src/views/codeNotes.vue
+++ src/views/codeNotes.vue
@@ -2,16 +2,16 @@
   <v-container class="d-flex flex-column align-center w-100">
     <BackButton />
     <h1 class="my-4 text-center text-primary">Code Notes</h1>
-    <v-btn color="primary" dark @click="openNewNoteDialog" class="mb-4">Add Note</v-btn>
+    <v-btn color="primary" dark @click="openNewNoteDialog" class="mb-4">Add +</v-btn>
     <v-dialog v-model="showDialog" max-width="600">
-      <v-card>
+      <v-card color="indigo darken-4">
         <v-card-title>
           <span class="headline">{{ dialogTitle }}</span>
         </v-card-title>
         <v-card-text>
           <v-form>
             <v-text-field v-model="note.title" label="Title"></v-text-field>
-            <v-select
+            <v-select 
               v-model="note.language"
               :items="languages"
               item-text="text"
@@ -23,24 +23,22 @@
             <v-textarea v-model="note.content" label="</>" auto-grow></v-textarea>
           </v-form>
         </v-card-text>
-        <v-card-actions>
+        <v-card-actions class="white--text">
           <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="closeDialog">Cancel</v-btn>
-          <v-btn color="blue darken-1" text @click="saveNote">Save</v-btn>
+          <v-btn color="blue-grey darken-3" text @click="closeDialog">Cancel</v-btn>
+          <v-btn class="bg-green darken-2" @click="saveNote">Save</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
     <v-row class="w-100" justify="center">
       <v-col cols="12" md="4" lg="3" v-for="(note, index) in notes" :key="index" class="m-2" >
-        <v-card class="d-flex flex-column align-center note" :style="{ backgroundColor: note.color }">
-          <v-card-title>
-             {{ note.title }}
-            <v-spacer></v-spacer>
-          </v-card-title>
-          <v-spacer></v-spacer>
-          <v-icon>{{ note.language.icon }}</v-icon>
-          <v-card-actions>
-            <v-btn text @click="editNote(index)">Edit</v-btn>
+        <v-card class="pa-2 d-flex flex-column align-center color-white" :style="{ backgroundColor: note.color }">
+          <v-card-title class="text-center text-wrap text-h5"> {{ note.title }} </v-card-title>
+          <v-icon size="36"> {{ note.language.icon }} </v-icon>
+          <v-card-actions class="d-flex justify-space-between">
+            <v-btn text @click="editNote(index)">
+              <v-icon>mdi-lead-pencil</v-icon>
+            </v-btn>
             <v-btn icon @click.stop="deleteNote(index)">
               <v-icon>mdi-delete</v-icon>
             </v-btn>
@@ -136,9 +134,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-.note {
-  height: 200px;
-}
-</style>
